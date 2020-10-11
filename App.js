865e5a8feeb3769be3dbb332312a8aa8c9b78b67
daovinhlinh <linh142000@gmail.com>
2020-10-11 22:11:34 +0700

    add

--- App.js
+++ App.js
@@ -6,21 +6,58 @@ import {
     View,
     FlatList,
     ActivityIndicator,
+    Linking,
 } from "react-native";
 import NewsCard from "./components/NewsCard";
 
-export default function App() {
-    const [data, setData] = useState();
+const filterArticle = (arr) => {
+    const filtered = [];
+    arr.forEach((item) => {
+        let unique = true;
+        filtered.forEach((item1) => {
+            const isEqual = JSON.stringify(item) === JSON.stringify(item1);
+            if (isEqual) unique = false;
+        });
+        if (unique) filtered.push(item);
+    });
+    return filtered;
+};
+
+const onPress = (url) => {
+    Linking.canOpenURL(url).then((supported) => {
+        if (supported) {
+            Linking.openURL(url);
+        } else {
+            console.log(`Don't know how to open URL: ${url}`);
+        }
+    });
+};
+
+export default function App(props) {
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [hasError, setHasError] = useState(false);
+    const [lastPageReached, setLastPageReached] = useState(false);
 
     const getNew = async () => {
         const key = "008ae0a0c5a64ce08f03fe9bda99b7cb";
-        const api = `http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${key}`;
-        const response = await fetch(api);
-        const news = await response.json();
-        setData(news.articles);
+        try {
+            const api = `https://newsapi.org/v2/top-headlines?country=us&page=${page}&pageSize=5&apiKey=${key}`;
+            const response = await fetch(api);
+            const news = await response.json();
+            const moreArticle = news.articles.length > 0;
+            if (moreArticle) {
+                const filteredList = filterArticle([...data, ...news.articles]);
+                setData(filteredList);
+                setPage(page + 1);
+            } else {
+                setLastPageReached(true);
+            }
+        } catch (error) {
+            setHasError(true);
+        }
         setLoading(false);
-        console.log(news.articles);
     };
 
     useEffect(() => {
@@ -29,22 +66,46 @@ export default function App() {
 
     if (loading) {
         return (
-            <View>
-                <ActivityIndicator size="large" loading={loading} />
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="#0000ff" />
             </View>
         );
     }
 
-    const renderItem = ({ item }) => <NewsCard data={item} />;
+    if (hasError) {
+        return (
+            <View style={styles.container}>
+                <Text>{`Has Error :-(`}</Text>
+            </View>
+        );
+    }
+
+    const renderItem = ({ item }) => (
+        <NewsCard data={item} onPress={() => onPress(item.url)} />
+    );
 
     return (
         <View style={styles.container}>
+            <View style={styles.row}>
+                <Text style={styles.label}>Articles Count: </Text>
+                <Text style={styles.info}>{data.length}</Text>
+            </View>
             <FlatList
                 data={data}
                 renderItem={renderItem}
                 keyExtractor={(item, index) => `${index}`}
+                onEndReached={getNew}
+                onEndReachedThreshold={1}
+                ListFooterComponent={
+                    lastPageReached ? (
+                        <View style={{ alignItems: "center" }}>
+                            <Text>No more articles</Text>
+                        </View>
+                    ) : (
+                        <ActivityIndicator size="large" color="#0000ff" />
+                    )
+                }
             />
-            <NewsCard data={data} />
         </View>
     );
 }
@@ -55,5 +116,23 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
+        paddingTop: 30,
+    },
+    row: {
+        flexDirection: "row",
+    },
+    label: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    info: {
+        fontSize: 18,
+    },
+    button: {
+        backgroundColor: "aqua",
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        margin: 20,
+        borderRadius: 15,
     },
 });
--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,30 +1,53 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
-
+import moment from "moment";
+import { Card, Button } from "react-native-elements";
 const { width, height } = Dimensions.get("window");
+import { Icon } from "react-native-elements";
 
-export default function NewsCard({ data }) {
+export default function NewsCard({ onPress, data }) {
     // const { data } = props;
-    const { title, author, description, content, img } = data;
+    const {
+        title,
+        author,
+        description,
+        content,
+        urlToImage,
+        publishedAt,
+    } = data;
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text>{title}</Text>
+            <View style={styles.headerContainer}>
+                <Text style={styles.header}>{title}</Text>
                 <Image
                     style={styles.image}
                     source={{
-                        uri: img,
+                        uri: urlToImage,
                     }}
                 />
             </View>
+
             <View style={[styles.content, styles.size]}>
-                <Text
-                    style={[styles.text, { color: "gray", fontWeight: "bold" }]}
-                >
-                    {author}
-                </Text>
+                <Text style={styles.label}>Written by {author}</Text>
                 <Text style={styles.text}>{description}</Text>
                 <Text style={styles.text}>{content}</Text>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Text style={styles.label}>
+                        Published {moment(publishedAt).format("LLL")}
+                    </Text>
+                </View>
+                <Button
+                    icon={<Icon />}
+                    title="Read more"
+                    backgroundColor="#03A9F4"
+                    style={{ width: width * 0.9 }}
+                    onPress={onPress}
+                />
             </View>
         </View>
     );
@@ -33,16 +56,29 @@ export default function NewsCard({ data }) {
 const styles = StyleSheet.create({
     container: {
         alignItems: "center",
-        borderWidth: 1,
-        borderColor: "black",
+        borderBottomWidth: 0.8,
+        borderBottomColor: "gray",
         borderRadius: 10,
         padding: 10,
     },
     image: {
         width: width * 0.9,
-        height: height * 0.2,
+        height: height * 0.3,
     },
     header: {
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 20,
+    },
+    label: {
+        fontSize: 19,
+        marginBottom: 5,
+        color: "#7b7b7b",
+        fontWeight: "bold",
+        textAlign: "center",
+        padding: 10,
+    },
+    headerContainer: {
         alignItems: "center",
     },
     size: {
@@ -53,7 +89,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     text: {
-        fontSize: 18,
+        fontSize: 17,
         marginBottom: 5,
     },
 });
